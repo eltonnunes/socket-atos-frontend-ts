8038b3cddf9aa0df50f282b2d3887a4c05a7619d
Italo Mota <italo.mota@atoscapital.com.br>
2019-08-23 18:31:34 -0300

    Inicia implementação frontend com typescript - POSSUI ERROS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import socketIOClient from 'socket.io-client'
 
-const App: React.FC = () => {
+export default function App() {
+  const [io, setIO] = useState(socketIOClient('http://192.168.0.25:3333'))
+  const [mensagens, setMensagens] = useState([])
+  const [novaMensagem, setNovaMensagem] = useState('')
+  const [sala, setSala] = useState(false)
+  const [idsConectados, setIdsConectados] = useState([])
+  const [meuId, setMeuId] = useState('')
+
+  function enviarMensagem() {
+    // setMensagens([...mensagens, novaMensagem])
+    setMensagens(mensagens.push(novaMensagem))
+
+    io.emit('novaMensagem', novaMensagem)
+
+    setNovaMensagem('')
+  }
+
+  function enviarMensagemSala() {
+    io.emit('novaMensagemSala', novaMensagem)
+
+    setMensagem('')
+  }
+
+  function enviarMensagemId(id: number) {
+    // setMensagens([...mensagens, novaMensagem])
+    setMensagens(mensagens.concat[novaMensagem])
+
+    io.emit('novaMensagemId', id, novaMensagem)
+
+    setMensagem('')
+  }
+
+  function ouvirMensagens() {
+    io.on('mensagens', novaMensagem => {
+      setMensagens([...mensagens, novaMensagem])
+    })
+
+    io.on('sala', () => {
+      setSala(!state.sala)
+    })
+
+    io.on('idsConectados', idsConectados => {
+      setIdsConectados(idsConectados)
+    })
+
+    io.on('connect', () => {
+      setMeuId(io.id)
+    })
+  }
+
+  useEffect(() => {
+    ouvirMensagens()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h2>{meuId}</h2>
 
-export default App;
+      {!sala && (
+        <button onClick={() => io.emit('entrarSala', '1107')}>
+          Entrar na sala
+        </button>
+      )}
+
+      {sala && (
+        <button onClick={() => io.emit('sairSala', '1107')}>
+          Sair da sala
+        </button>
+      )}
+
+      <ul>
+        {mensagens.map((mensagem, index) => (
+          <li key={index}>{mensagem}</li>
+        ))}
+      </ul>
+
+      <input
+        autoFocus
+        value={novaMensagem}
+        onChange={e => setNovaMensagem(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={enviarMensagem}>
+        Enviar
+      </button>
+
+      <br />
+
+      <button onClick={enviarMensagemSala}>
+        Enviar para sala
+      </button>
+
+      <br />
+
+      {idsConectados.map((id, index) => (
+        <div key={index}>
+          <button onClick={() => enviarMensagemId(id)}>
+            Enviar para {id}
+          </button>
+
+          <br />
+        </div>
+      ))}
+      
+    </>
+  )
+}
