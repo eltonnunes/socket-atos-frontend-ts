abfa820ce9719488af7f0d54c4cad29123095361
Elton Nunes <ltnnns@gmail.com>
2019-08-27 15:27:04 -0300

    Ajustes no FrontEnd
    Adicionando tipo de dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import socketIOClient from 'socket.io-client'
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
 
 export default function App() {
-  const [io, setIO] = useState(socketIOClient('http://192.168.0.25:3333'))
-  const [mensagens, setMensagens] = useState([])
-  const [novaMensagem, setNovaMensagem] = useState('')
-  const [sala, setSala] = useState(false)
-  const [idsConectados, setIdsConectados] = useState([])
-  const [meuId, setMeuId] = useState('')
+  const [io] = useState(socketIOClient('https://pohs3.sse.codesandbox.io/'));
+  const [mensagens, setMensagens] = useState<Array<string>>([]);
+  const [novaMensagem, setNovaMensagem] = useState<string>('');
+  const [sala, setSala] = useState<Boolean>(false);
+  const [idsConectados, setIdsConectados] = useState<Array<string>>([]);
+  const [meuId, setMeuId] = useState<string>('');
 
   function enviarMensagem() {
-    // setMensagens([...mensagens, novaMensagem])
-    setMensagens(mensagens.push(novaMensagem))
-
-    io.emit('novaMensagem', novaMensagem)
-
-    setNovaMensagem('')
+    setMensagens([...mensagens, novaMensagem]);
+    io.emit('novaMensagem', novaMensagem);
+    setNovaMensagem('');
   }
 
   function enviarMensagemSala() {
-    io.emit('novaMensagemSala', novaMensagem)
-
-    setMensagem('')
+    io.emit('novaMensagemSala', novaMensagem);
+    setNovaMensagem('');
   }
 
-  function enviarMensagemId(id: number) {
-    // setMensagens([...mensagens, novaMensagem])
-    setMensagens(mensagens.concat[novaMensagem])
-
-    io.emit('novaMensagemId', id, novaMensagem)
-
-    setMensagem('')
-  }
-
-  function ouvirMensagens() {
-    io.on('mensagens', novaMensagem => {
-      setMensagens([...mensagens, novaMensagem])
-    })
-
-    io.on('sala', () => {
-      setSala(!state.sala)
-    })
-
-    io.on('idsConectados', idsConectados => {
-      setIdsConectados(idsConectados)
-    })
-
-    io.on('connect', () => {
-      setMeuId(io.id)
-    })
+  function enviarMensagemId(id: string) {
+    setMensagens([...mensagens, novaMensagem]);
+    io.emit('novaMensagemId', id, novaMensagem);
+    setNovaMensagem('');
   }
 
   useEffect(() => {
-    ouvirMensagens()
-  }, [])
+    io.on('mensagens', (novaMensagem: string) => {
+      setMensagens([...mensagens, novaMensagem]);
+    });
+
+    io.on('sala', () => {
+      setSala(!sala);
+    });
+
+    io.on('idsConectados', (idsConectados: [string]) => {
+      setIdsConectados(idsConectados);
+    });
+
+    io.on('connect', () => {
+      setMeuId(io.id);
+    });
+  }, [io, sala, mensagens]);
 
   return (
     <>
-      <h2>{meuId}</h2>
-
+      <h2>{meuId}</h2> - <pre>IDS: {idsConectados.length}</pre>
       {!sala && (
         <button onClick={() => io.emit('entrarSala', '1107')}>
           Entrar na sala
         </button>
       )}
-
       {sala && (
         <button onClick={() => io.emit('sairSala', '1107')}>
-          Sair da sala
+          Sair da sal
         </button>
       )}
-
       <ul>
         {mensagens.map((mensagem, index) => (
           <li key={index}>{mensagem}</li>
         ))}
       </ul>
-
       <input
         autoFocus
         value={novaMensagem}
         onChange={e => setNovaMensagem(e.target.value)}
       />
-
-      <br /><br />
-
-      <button onClick={enviarMensagem}>
-        Enviar
-      </button>
-
       <br />
-
-      <button onClick={enviarMensagemSala}>
-        Enviar para sala
-      </button>
-
       <br />
-
+      <button onClick={enviarMensagem}>Enviar</button>
+      <br />
+      <button onClick={enviarMensagemSala}>Enviar para sala</button>
+      <br />
       {idsConectados.map((id, index) => (
         <div key={index}>
-          <button onClick={() => enviarMensagemId(id)}>
-            Enviar para {id}
-          </button>
+          <button onClick={() => enviarMensagemId(id)}>Enviar para {id}</button>
 
           <br />
         </div>
       ))}
-      
     </>
-  )
+  );
 }
